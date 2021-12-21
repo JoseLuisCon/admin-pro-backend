fe751a2691b8edb19196896cca69740b649c59cf
JoseLuisCon <condepa45@gmail.com>
2021-12-21 16:51:42 +0100

    Primer Commit

---
+++ database/config.js
@@ -0,0 +1,23 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+require ('dotenv').config();
+
+const dbConnection = async () => {
+
+    try {
+        await mongoose.connect(process.env.DB_CONNECT,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                         });
+        console.log('Base de datos Online');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a  la hora de iniciar la BD en Mongo')
+    }
+
+}
+
+module.exports = { dbConnection }
+
